284eb7ac897470f01b63338e91589e5eaf0c9a62
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-09-26 10:52:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math;
-volume = float(input("Введите объём: "));
-radius = (3*(volume**(1/3)))/(4* math.pi);
-print("Радиус равен: ", radius);
+import math
+volume = float(input("Введите объём: "))
+radius = (3*(volume**(1/3)))/(4* math.pi)
+print("Радиус равен: ", radius)
